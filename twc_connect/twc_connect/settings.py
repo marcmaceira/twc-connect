30c097f51c92b8cd7ca92d6db73ee43943e803a1
Marc A. Maceira Zayas <mmaceira.zayas@gmail.com>
2020-08-05 15:57:51 -0700

    Add gunicorn and django-heroku

--- twc_connect/twc_connect/settings.py
+++ twc_connect/twc_connect/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -120,3 +121,4 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+django_heroku.settings(locals())
