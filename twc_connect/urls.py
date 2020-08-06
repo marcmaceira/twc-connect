c03145a049d212d849b504fef1050bae1fc6b359
Marc Maceira <mmaceira.zayas@gmail.com>
2020-08-05 22:57:57 -0400

    Add robots.txt

--- twc_connect/urls.py
+++ twc_connect/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from events import views
+from django.conf.urls import url
+from django.http import HttpResponse
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name = "home"),
+    url(r'^robots.txt', lambda x: HttpResponse("User-Agent: *\nDisallow: /", content_type="text/plain"), name="robots_file"),
 ]
